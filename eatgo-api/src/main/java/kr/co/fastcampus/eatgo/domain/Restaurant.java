594ceb7dcbecc2183518eafff50839db733574ba
ijnuemik <43838237+ijnuemik@users.noreply.github.com>
2020-07-17 02:20:00 +0900

    Initial Commit

---
+++ eatgo-api/src/main/java/kr/co/fastcampus/eatgo/domain/Restaurant.java
@@ -0,0 +1,29 @@
+package kr.co.fastcampus.eatgo.domain;
+
+public class Restaurant {
+    private final String name;
+    private final String address;
+    private final Long id;
+
+    public Restaurant(Long id, String name, String address) {
+        this.name = name;
+        this.address = address;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getInformation(){
+        return name + " in " + address;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
---
+++ eatgo-api/src/main/java/kr/co/fastcampus/eatgo/interfaces/RestaurantController.java
@@ -0,0 +1,32 @@
+package kr.co.fastcampus.eatgo.interfaces;
+
+import kr.co.fastcampus.eatgo.domain.Restaurant;
+import kr.co.fastcampus.eatgo.domain.RestaurantRepository;
+import kr.co.fastcampus.eatgo.domain.RestaurantRepositoryImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class RestaurantController {
+
+    @Autowired
+    private RestaurantRepository repository = new RestaurantRepositoryImpl();
+
+    @GetMapping("/restaurants")
+    public List<Restaurant> list(){
+        List<Restaurant> restaurants = repository.findAll();
+
+        return restaurants;
+    }
+
+    @GetMapping("/restaurants/{id}")
+    public Restaurant detail(@PathVariable("id") Long id){
+        Restaurant restaurant = repository.findByid(id);
+
+        return restaurant;
+    }
+}
